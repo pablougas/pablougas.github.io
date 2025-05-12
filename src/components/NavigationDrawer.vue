f04ec82b79dcf3ad3997178ef1500477a026e687
Pablo Ugas <pablougas@me.com>
2025-05-12 16:16:47 -0700

    refactor portfolio

---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app-bar v-if="smAndDown" :elevation="2">
+    <template #prepend>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+    </template>
+
+  </v-app-bar>
+  <v-navigation-drawer
+    v-model="drawer"
+    permanent
+  >
+    <v-list>
+      <v-list-item>
+        <v-card height="160" width="160">
+          <v-img cover src="@/assets/me.jpg" />
+        </v-card>
+      </v-list-item>
+    </v-list>
+
+    <v-divider />
+
+    <v-list density="compact" nav>
+      <v-list-item
+        prepend-icon="mdi-home"
+        title="Home"
+        :to="{ path: '/' }"
+      />
+      <v-list-item
+        prepend-icon="mdi-account-circle"
+        title="About Me"
+        :to="{ path: 'about' }"
+      />
+      <v-list-item
+        prepend-icon="mdi-folder-heart-outline"
+        title="Projects"
+        value="users"
+      />
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue'
+  import { useDisplay } from 'vuetify';
+
+  const { smAndDown, mdAndUp } = useDisplay()
+  const drawer = ref(mdAndUp)
+</script>
---
+++ src/pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container class="fill-height">
+    <v-row align-content="center" class="fill-height">
+
+      <h3 class="text-h3 mb-2">Hello</h3>
+      <p class="text-body2">
+        Based in Seattle, Washington, I’m a naturally curious person who is always looking for ways to learn and grow.
+        Whether it’s exploring new technologies, diving into complex problems, or expanding my knowledge in unexpected
+        areas, I find joy in the process of discovery. I’m constantly seeking to improve and evolve, both personally
+        and professionally. I believe that learning is a lifelong journey, and I’m passionate about embracing new
+        challenges that help me push my boundaries.
+        <br>
+        <br>
+        I love building things, web apps that users love, wood working projects around the house for extra storage,
+        but when I'm not building something you can find me usually riding bikes with my kids (when the weather permits),
+        diving into their latest curiousity or hobbies (minecraft, rocks and soccer). Much like our house, I thrive in
+        environments that encourage growth and foster innovation.
+      </p>
+    </v-row>
+  </v-container>
+</template>
